1d6d6a2e39208d2390faadd1efb55e8ef3487116
Jorman <tille100@gmail.com>
2012-11-07 14:13:25 -0500

    challenge5 complete

---
+++ challenge5/README.md
@@ -0,0 +1,26 @@
+RETO 5
+======
+
+Desplegar el proyecto en NetBeans IDE 7.2.1
+
+-Agregar las librerias xstream-1.4.3.jar y xmlpull-1.1.3.1.jar al proyecto RobotCalc.
+
+-Agregar la libreria jdom.jar al proyecto WebCalc.
+
+-Antes de compilar debe buscar en el proyecto llamado RobotCalc, el paquete Controller donde se encuentra
+la clase CalcProxy.java en donde debe colocarse la ruta que conduce al archivo CalcServer.xml, que viene dentro
+de la carpeta del proyecto.
+
+-Con el proyecto WebCalc debe hacerse exactamente lo mismo que con el proyecto anterior, solo que el archivo 
+CalcServer.xml es diferente del anterior y esta contenido dentro de la carpeta de este proyecto. 
+
+-Para correr el proyecto primero deben desplegarse AppServer1 y AppServer2.
+
+-Para ver como funciona el servicio con RobotCalc, se debe abrir con netBeans el archivo CalcServer.xml, que
+es en donde se modifican los valores que queremos operar y donde posteriormente se vera el resultado y luego
+ejecutar la clase Test.java
+
+-Para WebCalc se debe ejecutar Calc.html, que desplegara una pagina en internet donde solo se debe proceder
+a ingresar los datos y el operador de la operacion que se quiera realizar.
+
+
---
+++ challenge5/src/RobotCalc/src/java/Test/Test.java
@@ -0,0 +1,47 @@
+package Test;
+
+import Controller.CalcProxy;
+import Logic.XMLProcessing.CalcDTO.WebService;
+import Logic.XMLProcessing.CalcDTO.WebServices;
+import Logic.XMLProcessing.XMLOperationsSingleton;
+import Logic.XMLProcessing.XMLTransformations.XMLOperations;
+
+/**
+ *
+ * @author HORUS793
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        XMLOperationsSingleton xmlSingleton = XMLOperationsSingleton.getInstance();
+        String strFileName = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\RobotCalc\\CalcServer.xml";
+        WebServices webServices = xmlSingleton.getWebServices(strFileName);
+        for (WebService webService : webServices.getWebServices()) {
+            String strP1 = webService.getParams().getP1();
+            String strP2 = webService.getParams().getP2();
+            float nmFirst = Float.parseFloat(strP1);
+            float nmSecond = Float.parseFloat(strP2);
+            String strResult = String.valueOf(ExecuteOperation(nmFirst, nmSecond, webService.getMethod()));
+            webService.setResultado(strResult);
+        }
+        XMLOperations.WriteXMLFile(strFileName, xmlSingleton.getDOMWebServices(webServices));
+    }
+    
+    public static float ExecuteOperation(float nmFirst, float nmSecond, String strOperator) {
+        float result = 0;
+        CalcProxy proxy = new CalcProxy();
+        if(strOperator != null && !strOperator.equals("")) {
+            char chOperator = strOperator.charAt(0);
+            if(chOperator ==  '+') {
+                result = proxy.Add(nmFirst,nmSecond);
+            } else if(chOperator ==  '-') {
+                result = proxy.Substract(nmFirst, nmSecond);
+            } else if(chOperator ==  '*') {
+                result = proxy.Multiply(nmFirst, nmSecond);
+            } else if(chOperator ==  '/') {
+                result = proxy.Divide(nmFirst, nmSecond);
+            }
+        }
+        return result;
+    }
+}
